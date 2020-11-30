207cd25b8457be2ae89d72154dd21430d062d087
LeonardoQui <leoqp123r@gmail.com>
2020-11-30 18:52:31 -0500

    Imports y arreglos

--- api.py
+++ api.py
@@ -1,12 +1,17 @@
-from flask import Flask, jsonify
+import flask
+import json
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
+DEBUG=True
+
 app = Flask(__name__)
+
 app.config.from_object(__name__)
 
-CORS(app, resources={r'/*': {'origins': '*'}})
+CORS(app)
 
-@app.route('/productos',methods=['GET'])
+@app.route('/Productos',methods=['GET'])
 def Mostrar():
         return jsonify({[{"id": 1,"nombre": "El lllsexto sentido","director": "M. Night Shyamalan","clasificacion": "Drama"}]})
 
@@ -15,4 +20,4 @@ def Crear():
         return "Producto creado";
 
 if __name__=="__main__":
- app.run(host = '0.0.0.0')
+ app.run(host = '0.0.0.0', debug=True)
