cc691497c3d38e66bb2caae6c8688d4fb2380d75
LeonardoQui <leoqp123r@gmail.com>
2020-11-30 15:58:00 -0500

    creacion prueba api

---
+++ api.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/productos',methods=['GET'])
+def Mostrar():
+        return "Mostrando productos";
+
+@app.route('/crear', methods=['POST'])
+def Crear():
+        return "Producto creado";
+
+if __name__=="__main__":
+ app.run(host = '0.0.0.0')
