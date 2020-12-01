8084b997bd3adce4717d963be74b24a2012fded4
LeonardoQui <leoqp123r@gmail.com>
2020-12-01 14:45:36 -0500

    Cors de prueba

--- api.py
+++ api.py
@@ -8,10 +8,7 @@ app = Flask(__name__)
 
 app.config.from_object(__name__)
 
-CORS(app)
-@app.route('/')
-def hellos():
-	return "Hello friend"
+CORS(app, resources={r'/*': {'origins': '*'}})
 
 @app.route('/ping', methods=['GET'])
 def ping_pong():
