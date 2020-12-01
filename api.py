428df8ce6cacbf12943e972d1372e907083bcdf4
LeonardoQui <leoqp123r@gmail.com>
2020-11-30 19:36:05 -0500

    jhguyfcd6

--- api.py
+++ api.py
@@ -5,8 +5,26 @@ app = Flask(__name__)
 
 @app.route('/ping', methods=['GET'])
 def ping_pong():
-    return jsonify('pong!')
+	BOOKS = [
+    {
+        'title': 'On the Road',
+        'author': 'Jack Kerouac',
+        'read': True
+    },
+    {
+        'title': 'Harry Potter and the Philosopher\'s Stone',
+        'author': 'J. K. Rowling',
+        'read': False
+    },
+    {
+        'title': 'Green Eggs and Ham',
+        'author': 'Dr. Seuss',
+        'read': True
+    }
+	]
+    return jsonify(BOOKS)
 
 
 if __name__ == '__main__':
     app.run( host = '0.0.0.0')
+
