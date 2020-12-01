838f2c09d7d83cdda5dba0eb692250bc12c4f2b7
LeonardoQui <leoqp123r@gmail.com>
2020-11-30 19:26:26 -0500

    Prueba desperada

--- api.py
+++ api.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
-import json
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
 from flask_cors import CORS
 
-DEBUG=True
 
+# configuration
+DEBUG = True
+
+# instantiate the app
 app = Flask(__name__)
-
 app.config.from_object(__name__)
 
-CORS(app)
+# enable CORS
+CORS(app, resources={r'/*': {'origins': '*'}})
 
-@app.route('/Productos',methods=['GET'])
-def Mostrar():
-        return "Perra vida";
 
-@app.route('/crear', methods=['POST'])
-def Crear():
-        return "Producto creado";
+# sanity check route
+@app.route('/ping', methods=['GET'])
+def ping_pong():
+    return jsonify('pong!')
 
-if __name__=="__main__":
- app.run(host = '0.0.0.0', debug=True)
+
+if __name__ == '__main__':
+    app.run( host = '0.0.0.0')
